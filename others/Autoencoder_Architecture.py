b207cd603f53531da2213207376773105bfba72a
Jayden <administrator418@users.noreply.github.com>
2024-12-04 12:31:16 +0800

    1

--- others/Autoencoder_Architecture.py
+++ others/Autoencoder_Architecture.py
@@ -696,7 +696,7 @@ def save_weights(epoch, autoencoder, delete_previous=False):
         except:
             pass  
     
-    autoencoder.save_weights(f'{path_to_store_results}/{timestamp}/checkpoints/autoencoder/epoch_{epoch}')
+    autoencoder.save_weights(f'{path_to_store_results}/{timestamp}/checkpoints/autoencoder/epoch_{epoch}.weights.h5')
 
 #################################################################Training_Loop########################################################
 if __name__ == "__main__":
